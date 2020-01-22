90f03cee5bbe470201cd83f0c31c78ac714f9c15
Jessica Johnson <dev@sqldataninja.com>
2020-01-21 22:06:45 -0500

    inital control table creation and extracting schema creation

---
+++ cloud-data-warehouse/contoso-dw/ctrl/ctrl.data_pipeline_log.sql
@@ -0,0 +1,29 @@
+﻿CREATE TABLE [ctrl].[data_pipeline_log]
+(
+	log_id						INT				NOT NULL	IDENTITY(1,1),
+	process_id					INT				NOT NULL,
+	run_id						INT				NOT NULL,
+	source_data_store			NVARCHAR(500)	NULL,
+	target_data_store			NVARCHAR(500)	NULL,
+	source_row_cnt				BIGINT			NULL,
+	target_initial_row_cnt		BIGINT			NULL,
+	target_final_row_cnt		BIGINT			NULL,
+	insert_target_row_cnt		BIGINT			NULL,
+	update_targtt_row_cnt		BIGINT			NULL,
+	error_row_cnt				BIGINT			NULL,
+	final_tgt_row_cnt			BIGINT			NULL,
+	table_max_key				BIGINT			NULL,
+	addtl_info_msg				NVARCHAR(4000)	NULL,
+	error_code					NVARCHAR(150)	NULL,
+	error_msg					NVARCHAR(4000)	NULL,
+	error_descr					NVARCHAR(4000)	NULL,						--	The description of the error if the process is failed.
+	is_successful				BIT				NULL,
+	created_dttm				DATETIME		NOT NULL,
+	modified_dttm				DATETIME		NULL
+)
+WITH
+(
+    DISTRIBUTION = HASH (process_id),
+    CLUSTERED COLUMNSTORE INDEX
+)
+GO
---
+++ cloud-data-warehouse/contoso-dw/ctrl/ctrl.data_pipeline_run.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [ctrl].[data_pipeline_run]
+(
+	run_id				INT				NOT NULL	IDENTITY(1,1),	--	run Identifier, this will be generated sequentially.
+	process_id			INT				NOT NULL,
+	data_pipeline_id	NVARCHAR(50)	NOT NULL,
+	execution_order		INT				NULL,
+	run_start_dttm		DATETIME		NOT NULL,					--	Start DateTime of the run.
+	run_end_dttm		DATETIME		NULL,						--	End DateTime of the run.
+	env_cd				NVARCHAR(25)	NOT NULL,					--	Environment of run (dev, test, prod)
+	created_dttm		DATETIME		NOT NULL,
+	modified_dttm		DATETIME		NULL
+
+)
+WITH
+(
+    DISTRIBUTION = ROUND_ROBIN,
+    CLUSTERED COLUMNSTORE INDEX
+)
+GO
